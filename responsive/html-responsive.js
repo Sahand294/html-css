9fcd259768e20b4fdd68c84f67d47d4a9b04524d
farza_mggxnah <sahandha1391@gmail.com>
2025-04-07 17:29:16 -0230

    test

---
+++ responsive/html-responsive.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function () {
+    // alert("hello sahand");
+    const mobile_menu = document.getElementById("mobile_menu");
+    const nav_bar = document.querySelector(".nav-bar-menu");
+    mobile_menu.addEventListener("click", function() {
+        // nav_bar.classList.toggle("active")
+        alert("hi sepehrc")
+    })
+})
