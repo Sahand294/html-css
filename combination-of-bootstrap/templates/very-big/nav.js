515e8f7895d0c8fc8839b0bae0ecf6012e0b4663
farza_mggxnah <sahandha1391@gmail.com>
2025-04-10 16:12:40 -0230

    test

--- combination-of-bootstrap/templates/very-big/nav.js
+++ combination-of-bootstrap/templates/very-big/nav.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const trigger = document.getElementById("trigger")
-    const target = document.querySelector(".nav-options-container")
-    trigger.addEventListener("click", function() {
-        target.classList.toggle("active")
+    const nav_trigger = document.getElementById("nav-options-trigger")
+    const nav_target = document.querySelector(".nav-options-container")
+    const info_trigger = document.getElementById("info-trigger")
+    const info_target = document.querySelector(".info")
+    nav_trigger.addEventListener("click", function() {
+        nav_target.classList.toggle("active")
+    })
+    info_trigger.addEventListener("click", function () {
+        info_target.classList.toggle("active")
     })
 })
